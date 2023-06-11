6ba3347cde0b2dd8ebeb786ffa7715842e0665a1
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2023-06-11 18:17:46 +0500

    Create number_sign_classification.dart

---
+++ Class-02/If-Else Statement/Practice-Example/number_sign_classification.dart
@@ -0,0 +1,14 @@
+void main(){
+// Practice Question: Number Sign Classification
+
+int number = -15; // Declare and initialize the number variable with a value of -15
+
+if (number > 0) { // Check if the number is greater than 0
+  print("Number is positive."); // Print "Number is positive." if the number is greater than zero
+} else if (number < 0) { // If the number is not greater than 0, check if it is less than 0
+  print("Number is negative."); // Print "Number is negative." if the number is less than zero
+} else { // If the number is not greater than 0 or less than 0
+  print("Number is zero."); // Print "Number is zero." if the number is zero
+}
+
+}
