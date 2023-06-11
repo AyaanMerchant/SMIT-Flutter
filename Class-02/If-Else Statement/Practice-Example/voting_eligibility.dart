7f051568406622bd60e48498a37eb33faa4eb21a
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2023-06-11 18:13:06 +0500

    Create voting_eligibility.dart

---
+++ Class-02/If-Else Statement/Practice-Example/voting_eligibility.dart
@@ -0,0 +1,13 @@
+void main(){
+// Practice Question: Voting Eligibility
+
+int age = 17; // Declare and initialize the age variable with a value of 17
+bool isCitizen = true; // Declare and initialize the isCitizen variable with a value of true
+
+if (age >= 18 && isCitizen) { // Check if the age is 18 or above and if the person is a citizen
+  print("You are eligible to vote."); // Print "You are eligible to vote." if the conditions are met
+} else {
+  print("You are not eligible to vote."); // Print "You are not eligible to vote." for any other case
+}
+
+}
