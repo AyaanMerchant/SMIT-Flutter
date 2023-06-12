4d36a6e1b4ad871e97f88bd3a2168f266e06c56c
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2023-06-12 13:34:05 +0500

    Create q6.dart

---
+++ Class-02/Assignment-01/q6.dart
@@ -0,0 +1,13 @@
+void main(){
+//Write a program to check whether an alphabet is a vowel or consonant.
+
+  String alphabet = 'a'; // The alphabet to be checked
+
+  if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' || alphabet == 'o' || alphabet == 'u') {
+    print("$alphabet is a vowel"); // Print if the alphabet is a vowel
+  } else {
+    print("$alphabet is a consonant"); // Print if the alphabet is a consonant
+  }
+  
+}
+
