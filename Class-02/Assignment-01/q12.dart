0bc2aa33813beb3493578e76d2a34aeb64c00919
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2023-06-13 01:36:32 +0500

    Create q12.dart

---
+++ Class-02/Assignment-01/q12.dart
@@ -0,0 +1,7 @@
+void main(){
+//Write a program to convert Celsius  to Fahrenheit   .
+//i.e: Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+  
+  double celsius;
+  double fahrenheit;
+}
