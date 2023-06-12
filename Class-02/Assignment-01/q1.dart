6c184f5bcd502ab3ff3fc2254c5ca2f65b69cdce
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2023-06-12 12:14:52 +0500

    Update q1.dart

--- Class-02/Assignment-01/q1.dart
+++ Class-02/Assignment-01/q1.dart
@@ -1,3 +1,17 @@
 void main(){
-
+//Create two integer variables length and breadth and assign values then check if they are square values or rectangle values.
+//ie: if both values are equal then it's square otherwise rectangle.
+  
+  // Create two integer variables length and breadth and assign values.
+  int length = 5; // Assign the value 5 to the variable "length".
+  int breadth = 7; // Assign the value 7 to the variable "breadth".
+  
+  // Check if length and breadth are square values or rectangle values.
+  if(length == breadth){ // If the length is equal to the breadth...
+    print("It's Square"); // ...print "It's Square".
+  }else{ // If the length is not equal to the breadth...
+    print("It's Rectangle"); // ...print "It's Rectangle".
+  }
+  
 }
+
