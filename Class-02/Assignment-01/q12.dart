7869bdb0423c204f8b37172b394d02b63adc3388
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2023-06-14 14:59:17 +0500

    Update q12.dart

--- Class-02/Assignment-01/q12.dart
+++ Class-02/Assignment-01/q12.dart
@@ -1,7 +1,9 @@
 void main(){
-//Write a program to convert Celsius  to Fahrenheit   .
-//i.e: Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+  // Write a program to convert Celsius to Fahrenheit.
+  // i.e: Temperature in degrees Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
   
-  double celsius;
-  double fahrenheit;
+  double celsius = 40; // Declare and assign the temperature in Celsius
+  double fahrenheit = (celsius * 9 / 5) + 32; // Convert Celsius to Fahrenheit using the conversion formula
+
+  print('The temperature in Fahrenheit is: $fahrenheit°F'); // Print the converted temperature in Fahrenheit
 }
