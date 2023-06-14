8367f84ed1c172c4304ccc6bc53145ed32eb83b4
Ayaan Merchant <70717456+AyaanMerchant@users.noreply.github.com>
2023-06-14 14:57:17 +0500

    Update q11.dart

--- Class-02/Assignment-01/q11.dart
+++ Class-02/Assignment-01/q11.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 void main(){
-//: Write a program to calculate root of any number.
-//i.e: √y = y½
-  
-  int y = 40;
-  
+  // Write a program to calculate the square root of a number.
+  // i.e: √y = y½
+
+  double number = 40; // Declare and assign the number for which we want to calculate the square root
+  double squareRoot = sqrt(number); // Calculate the square root using the sqrt() function from the dart:math library
+
+  print("The square root of $number is $squareRoot"); // Print the result
   
 }
