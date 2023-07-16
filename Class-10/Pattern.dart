2b91b9a797f390c4bd4e68e5adb2b15d2e285b93
AyaanMerchant <krmkae786@gmail.com>
2023-07-16 09:35:55 +0500

    Add Class-10 code

---
+++ Class-10/Pattern.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+// Pattern print
+
+  // var rows = 5;
+  // var counter = 1;
+  // var a = " ";
+  // for(var i =1; i < rows ; i++){
+  //   stdout.write("${a * (rows - i)}");
+  //   for(var j =1; j<=i ; j++){
+  //     stdout.write("${counter++} ");
+
+  //   }
+  //   print(" ");
+  // }
+
+  //TO DO LIST
+
+  //First make a proper structure ( like make functions etcc)
+
+  var inp = stdin.readLineSync();
+  print(inp);
+}
